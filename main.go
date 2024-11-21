9091d7307d19f465b1353c8ea7683b81f5f98000
Shopnil Shahriar <68482142+Shopnil09@users.noreply.github.com>
2024-11-20 19:11:33 -0500

    Learning pointers in Go

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt" // refer to documentation for what packages contain what types of f
 
 func main() {
 	// if you don't use a package or variable, Go will throw an error to avoid using dead code
-	const conferenceName string = "Go Conference"
+	// syntactic sugar in programming--a term to describe a feature in a language that let's you do something more easily
+	// const conferenceName string = "Go Conference"
+	// instead of assigning the variable with a keyword and type, you can declare it like this. Only appropriate for 'var' and not for 'const'. Does not work if you want to explicitly want to declare a type for the variable
+	conferenceName := "Go Conference"
 	// for such values that do not change aka const, you can use const keyword. var keyboard allows the variable to be changed
 	// if you try to change it, it will throw an error
 	const conferenceTickets int = 50
@@ -19,11 +22,20 @@ func main() {
 	// it is important to refer to documentation about data types to figure out which data type suits the variable better depending on the usercase
 	var userName string
 	var userTickets int
+	var emailAddress string
 	// initializing a variable to be filled later perhaps for user input
 	// just initializing the variable will throw a warning so you need to define the data types
 	// you need to tell Go Compiler the data type when declaring the variable--either by assigning a value or by declaring a type
-	userName = "Tom"
-	userTickets = 2
-	// using placeholders
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+	// for fmt.Scan() we need to understand pointer. What is a pointer?
+	// a pointer is a variable that points to the memory address of another variable
+	// instead of passing in the userName variable which is empty, we will pass in the pointer aka memory address for the userName variable so it can store the information there
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&userName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&emailAddress)
+	fmt.Println("Enter your ticket amount")
+	fmt.Scan(&userTickets)
+	fmt.Printf("Thank you, %v for booking %v tickets. We will send you a confirmation to your email %v\n", userName, userTickets, emailAddress)
+
 }
