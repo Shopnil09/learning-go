581596eca6b33b20fb7dc64e3729e2cd7b2e4664
Shopnil Shahriar <68482142+Shopnil09@users.noreply.github.com>
2024-11-22 21:08:53 -0500

    arrays, slices, and loops

--- main.go
+++ main.go
@@ -19,23 +19,43 @@ func main() {
 	// since we are not using println anymore, you have to add the \n separately for the newline
 	fmt.Printf("Get your tickets below for %v\n", conferenceName)
 
-	// it is important to refer to documentation about data types to figure out which data type suits the variable better depending on the usercase
-	var userName string
-	var userTickets int
-	var emailAddress string
+	// arrays where we have to specify the size
+	// var bookings [50] string
+	// slices where for dynamic lists
+	// what if you don't know what is the size of the array to begin with?
+	// Slices--a list that is more dynamic in size. An abstraction of array
+	var bookings []string
 	// initializing a variable to be filled later perhaps for user input
 	// just initializing the variable will throw a warning so you need to define the data types
 	// you need to tell Go Compiler the data type when declaring the variable--either by assigning a value or by declaring a type
 
-	// for fmt.Scan() we need to understand pointer. What is a pointer?
-	// a pointer is a variable that points to the memory address of another variable
-	// instead of passing in the userName variable which is empty, we will pass in the pointer aka memory address for the userName variable so it can store the information there
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&userName)
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&emailAddress)
-	fmt.Println("Enter your ticket amount")
-	fmt.Scan(&userTickets)
-	fmt.Printf("Thank you, %v for booking %v tickets. We will send you a confirmation to your email %v\n", userName, userTickets, emailAddress)
+	// infinite loop
+	for {
+		// for fmt.Scan() we need to understand pointer. What is a pointer?
+		// a pointer is a variable that points to the memory address of another variable
+		// instead of passing in the userName variable which is empty, we will pass in the pointer aka memory address for the userName variable so it can store the information there
+		// it is important to refer to documentation about data types to figure out which data type suits the variable better depending on the usercase
+		var userName string
+		var userTickets int
+		var emailAddress string
 
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&userName)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&emailAddress)
+		fmt.Println("Enter your ticket amount")
+		fmt.Scan(&userTickets)
+
+		// subtract the remaining ticket with the amount the user has requested to get the next remaining tickets
+		remainingTickets = remainingTickets - userTickets
+		// arrays and slices are common data structures in Go programming language
+		// cannot mix types in this array
+		bookings = append(bookings, userName)
+
+		fmt.Printf("Thank you, %v for booking %v tickets. We will send you a confirmation to your email %v\n", userName, userTickets, emailAddress)
+		fmt.Printf("There are %v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Whole array: %v\n", bookings)
+		// fmt.Printf("The first value: %v\n", bookings[0])
+		// loops are simplified in Go--you only have a for loop and you can use it for various use cases
+	}
 }
